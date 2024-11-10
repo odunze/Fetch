6c9fa889eebb94b5751850a91a75e5d806bb34ce
Lotanna Igwe-Odunze <47992836+odunze@users.noreply.github.com>
2024-11-10 17:38:28 -0500

    fixed scrolling

--- Fetch/Meal Detail/MealViewController.swift
+++ Fetch/Meal Detail/MealViewController.swift
@@ -11,6 +11,18 @@ class MealViewController: UIViewController {
 
     private var vm: MealDetailViewModel
     
+    var scroller: UIScrollView = {
+        let view = UIScrollView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     var nameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -19,16 +31,10 @@ class MealViewController: UIViewController {
     
     var instructionsView: UITextView = {
         let view = UITextView()
+        view.isScrollEnabled = false
+        view.font = .systemFont(ofSize: 16)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
-    }()
-
-    var amountStack: UIStackView = {
-        let stack = UIStackView()
-        stack.axis = .horizontal
-        stack.distribution = .fillProportionally
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        return stack
     }()
     
     var ingredientStack: UIStackView = {
@@ -68,13 +74,18 @@ class MealViewController: UIViewController {
         for ingredient in mealIngredients {
             let ingredientLabel = UILabel()
             let measurementLabel = UILabel()
+            
+            measurementLabel.numberOfLines = 2
+            measurementLabel.lineBreakMode = .byCharWrapping
+            measurementLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 40).isActive = true
 
             ingredientLabel.text = ingredient.name
             measurementLabel.text = ingredient.measure
 
             let measurementStack = UIStackView(arrangedSubviews: [ingredientLabel, measurementLabel])
             measurementStack.axis = .horizontal
-            
+            measurementStack.distribution = .equalCentering
+
             ingredientStack.addArrangedSubview(measurementStack)
         }
     }
@@ -84,24 +95,39 @@ class MealViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
         
         view.backgroundColor = .white
-
-        view.addSubview(nameLabel)
-        view.addSubview(ingredientStack)
-        view.addSubview(instructionsView)
+        
+        view.addSubview(scroller)
+        scroller.addSubview(contentView)
+        
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(ingredientStack)
+        contentView.addSubview(instructionsView)
+        
         
         NSLayoutConstraint.activate([
-            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            nameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            scroller.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scroller.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scroller.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scroller.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scroller.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scroller.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scroller.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scroller.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scroller.widthAnchor),
+            
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             
             ingredientStack.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20),
-            ingredientStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            ingredientStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            ingredientStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            ingredientStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
             
             instructionsView.topAnchor.constraint(equalTo: ingredientStack.bottomAnchor, constant: 20),
-            instructionsView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            instructionsView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            instructionsView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            instructionsView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            instructionsView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            instructionsView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20)
         ])
     }
     
